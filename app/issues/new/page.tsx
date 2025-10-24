bf54910cdccfa1b029b6ee1967677764f18bf6cb
Mohamed ayman <mohamed.ayman@lightbyte.me>
2025-10-24 21:41:46 +0300

    feat: add loading spinner to New Issue form submission button

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { TextField, Flex, Button, TextArea } from "@radix-ui/themes";
+import Spinner from "@/app/components/spinner";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -11,6 +12,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 type IssueProps = z.input<typeof issueSchema>;
 
 const NewIssue = () => {
+  const [loading, setLoading] = React.useState(false);
   const router = useRouter();
   const {
     register,
@@ -23,10 +25,14 @@ const NewIssue = () => {
   console.log(errors);
   const onSubmit = async (data: IssueProps) => {
     try {
+      setLoading(true);
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
+      setLoading(false);
       console.error("Failed to create issue:", error);
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -50,7 +56,7 @@ const NewIssue = () => {
         )}
       </Flex>
       <div className="mt-3">
-        <Button onClick={handleSubmit(onSubmit)}>Create Issue</Button>
+        <Button onClick={handleSubmit(onSubmit)} disabled={loading}>Create Issue {loading && <Spinner />}</Button>
       </div>
     </div>
   );
